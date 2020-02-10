81a9426ec9e4aa10e5bd4f0aff2e1c715bc6b81a
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-10 20:11:22 -0300

    Incluindo o programa mensagem_pessoal.py

---
+++ cap_02/mensagem_pessoal.py
@@ -0,0 +1,6 @@
+primeiro_nome = "andré"
+ultimo_nome = "stella"
+nome_completo = primeiro_nome + " " + ultimo_nome
+
+mensagem = "Alô " + nome_completo.title() + ", você gostaria de aprender um pouco de Python hoje?"
+print(mensagem)
