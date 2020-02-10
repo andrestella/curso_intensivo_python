7c68a134c81cf4d74ae973b3cef923cd3cba76b0
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-10 20:04:10 -0300

    Incluindo o programa acrescentar_espacos_branco.py

---
+++ cap_02/acrescentar_espacos_branco.py
@@ -0,0 +1,13 @@
+print("Python\n")
+
+# Tabulação
+print("\tPython\n")
+
+# Quebra de linha
+print("Languages:\nPython\nC\nJavaScript\n")
+
+# Quebra de linha e tabulação
+print("Languages:\n\tPython\n\tC\n\tJavaScript\n")
+
+# Tabulação e quebra de linha
+print("Languages:\t\nPython\t\nC\t\nJavaScript") 
