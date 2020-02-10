3482274c8d53512bfacb238d3e1bc006040bb1c0
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-10 20:06:13 -0300

    Incluindo o programa removendo_espacos_branco.py

---
+++ cap_02/removendo_espacos_branco.py
@@ -0,0 +1,5 @@
+nome = "\tAndré Stella\n"
+print(nome)
+print(nome.lstrip()) # ".lstrip()": remove espaços em branco do lado esquerdo de uma string
+print(nome.rstrip()) # ".rstrip()": remove espaços em branco do lado direito de uma string
+print(nome.strip()) # ".strip()": remove espaços em branco de ambos os lados de uma string
