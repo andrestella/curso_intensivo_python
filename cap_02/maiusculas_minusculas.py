0ac4662256d9b7e660bc0881c02783885dac7fe8
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-10 20:12:57 -0300

    Incluindo o programa maiusculas_minusculas.py

---
+++ cap_02/maiusculas_minusculas.py
@@ -0,0 +1,9 @@
+primeiro_nome = "aNdRé"
+segundo_nome = "aleXandre"
+ultimo_nome = "SteLLa"
+nome_completo = primeiro_nome + " " + segundo_nome + " " + ultimo_nome
+
+print(nome_completo)
+print(nome_completo.lower())
+print(nome_completo.upper())
+print(nome_completo.title())
