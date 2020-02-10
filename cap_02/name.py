3f9e83f1d4dc2227f66e875886087ec8e603a52c
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-10 20:00:26 -0300

    Incluindo o programa name.py

---
+++ cap_02/name.py
@@ -0,0 +1,5 @@
+name = "ADA lovelace"
+print(name)
+print(name.title()) # ".title()": exibe a primeira letra de cada palavra dentro da string com letra maiúscula
+print(name.upper()) # ".upper()": muda uma string para que tenha somente letras maiúsculas
+print(name.lower()) # ".lower()": muda uma string para que tenha somente letras minúsculas
