e04d635c6205ca6684259dcd43f6d1807789fd2d
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-11 13:48:32 -0300

    Incluindo o programa saudacoes.py

---
+++ cap_03/saudacoes.py
@@ -0,0 +1,7 @@
+nomes = ['andré', 'alexandre', 'stella']
+message = "Olá {}! Tudo bem?".format(nomes[0].title())
+print(message)
+message = "Olá {}! Tudo bem?".format(nomes[1].title())
+print(message)
+message = "Olá {}! Tudo bem?".format(nomes[2].title())
+print(message)
