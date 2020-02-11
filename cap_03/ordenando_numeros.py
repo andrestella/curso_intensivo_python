e8c06a9b26e0dedb564ca00adf86d4f3440791ad
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-11 19:34:21 -0300

    Incluindo o programa ordenando_numeros.py

---
+++ cap_03/ordenando_numeros.py
@@ -0,0 +1,19 @@
+numeros = [2, 8, 17, -5, 10, -15, 0, 6]
+print(numeros)
+numeros.sort()
+print(numeros)
+
+numeros = [2, 8, 17, -5, 10, -15, 0, 6]
+print(numeros)
+numeros.sort(reverse=True)
+print(numeros)
+
+numeros = [2, 8, 17, -5, 10, -15, 0, 6]
+print(numeros)
+print(sorted(numeros))
+print(numeros)
+print(sorted(numeros, reverse=True))
+print(numeros)
+
+numeros.reverse()
+print(numeros)
