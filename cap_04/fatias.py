b65b164eeeb748ac52c7f4935829f7d7880821a7
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-12 20:08:38 -0300

    Incluindo o programa fatias.py

---
+++ cap_04/fatias.py
@@ -0,0 +1,10 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print("Os três primeiros itens da lista são:")
+print(players[:3])
+
+print("\nTrês itens do meio da lista são:")
+print(players[1:4])
+
+print("\nOs três últimos itens da lista são:")
+print(players[-3:])
