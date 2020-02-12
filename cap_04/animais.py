16d415a10b3b342019c4e7fd201dd41276bdda16
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-12 14:31:26 -0300

    Incluindo o programa animais.py

---
+++ cap_04/animais.py
@@ -0,0 +1,4 @@
+animais = ['cachorro', 'gato', 'pássaro']
+for animal in animais:
+    print("Um {} seria um ótimo animal de estimação.".format(animal))
+print("Qualquer um desses animais seria um ótimo animal de estimação!")
