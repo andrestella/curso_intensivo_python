4712e8ada7e95a8cd4e767a248169aa4a327d430
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-13 18:56:07 -0300

    Incluindo o programa numeros_ordinais.py

---
+++ cap_05/numeros_ordinais.py
@@ -0,0 +1,11 @@
+numeros = list(range(1,10))
+
+for numero in numeros:
+    if numero == 1:
+        print("1st")
+    elif numero == 2:
+        print("2nd")
+    elif numero == 3:
+        print("3rd")
+    else:
+        print(str(numero) + "th")
