f0f8ec0bfac26de98d3b1d0e5b2b047694ef940a
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-12 21:25:07 -0300

    Incluindo o programa favorite_fruits.py

---
+++ cap_05/favorite_fruits.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['laranja', 'banana', 'maça']
+
+if 'laranja' in favorite_fruits:
+    print("Você realmente gosta de laranjas!")
+if 'abacaxi' in favorite_fruits:
+    print("Você realmente gosta de abacaxis!")
+if 'banana' in favorite_fruits:
+    print("Você realmente gosta de bananas!")
+if 'melancia' in favorite_fruits:
+    print("Você realmente gosta de melancias!")
+if 'maça' in favorite_fruits:
+    print("Você realmente gosta de maças!")
