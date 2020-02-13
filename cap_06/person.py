3327bf1bac0892f55708fe59b4e682a4df64be19
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-13 20:42:50 -0300

    Incluindo o programa person.py

---
+++ cap_06/person.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'andré',
+    'last_name': 'stella',
+    'age': 40,
+    'city': 'são paulo',
+}
+
+print(person['first_name'].title())
+print(person['last_name'].title())
+print(person['age'])
+print(person['city'].title())
