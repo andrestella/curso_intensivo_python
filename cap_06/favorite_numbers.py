a913bc51e65dbc6408832a171d1f70d9cf59e2bd
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-13 20:45:39 -0300

    Incluindo o programa favorite_numbers.py

---
+++ cap_06/favorite_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'andré': 40,
+    'andré_pai': 70,
+    'maria': 60,
+    'henri': 30,
+    'nádia': 35,
+    }
+
+print('André: {}'.format(favorite_numbers['andré']))
+print('André - pai: {}'.format(favorite_numbers['andré_pai']))
+print('Maria: {}'.format(favorite_numbers['maria']))
+print('Henri: {}'.format(favorite_numbers['henri']))
+print('Nádia: {}'.format(favorite_numbers['nádia']))
