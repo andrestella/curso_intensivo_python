1c2dfcd6d9ea6f23f97843ba1f6ee372759a84b1
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-15 19:25:02 -0300

    Incluindo o programa favorite_book.py

---
+++ cap_08/favorite_book.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    """ Exibe uma mensagem com o título do meu livro favorito. """
+    print('Um dos meus livros favoritos é "{}".'.format(title.title()))
+
+favorite_book("o mundo de sofia")
