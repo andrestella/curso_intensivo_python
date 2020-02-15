7e5895866ca921fc3b34645ff874f861d8ffee8f
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-14 21:21:52 -0300

    Modificando o laço while para utilizar uma flag

--- cap_07/parrot.py
+++ cap_07/parrot.py
@@ -1,8 +1,11 @@
 prompt = "\nTell me something, and I will repeat it back to you."
 prompt += "\n(Enter 'quit' to end the program): "
 
-message = ""
-while message != 'quit':
+# Usando uma flag (muitos eventos diferentes poderiam fazer o programa parar):
+active = True # flag de nome "active": controlará se o programa deve ou não continuar executando
+while active:
     message = input(prompt)
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    else:
         print(message)
