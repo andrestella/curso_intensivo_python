cc23496d314769834f429189f3f7b2722391e95e
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-15 19:12:29 -0300

    Incluindo o programa greeter2.py

---
+++ cap_08/greeter2.py
@@ -0,0 +1,8 @@
+def greet_user(username):
+    """ Exibe uma saudação simples. """
+    print("Hello, {}!".format(username.title()))
+
+greet_user("andré")
+
+# 'username' - parâmetro da função
+# 'andré' - argumento da função
