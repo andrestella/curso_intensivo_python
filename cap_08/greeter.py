a3c28827b42b992ca06670224e78b0c1eb8e0eaa
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-15 19:08:11 -0300

    Incluindo o programa greeter.py

---
+++ cap_08/greeter.py
@@ -0,0 +1,8 @@
+def greet_user():
+    """ Exibe uma saudação simples. """
+    # O texto acima é um comentário chamado 'docstring', que descreve o que a função faz
+    # As 'docstrings' são colocadas entre aspas triplas, que Python procura quando gera a 
+    # documentação das funções de seus programas
+    print("Hello!")
+
+greet_user() # chamada da função
