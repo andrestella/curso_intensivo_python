f1cbcbd046f8fe967a9d11bf57f8c514bd5d4d07
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-16 18:25:13 -0300

    Incluindo o programa printing_models2.py

---
+++ cap_08/printing_models2.py
@@ -0,0 +1,7 @@
+from printing_functions import print_models as pm, show_completed_models as sm
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pm(unprinted_designs, completed_models)
+sm(completed_models)
