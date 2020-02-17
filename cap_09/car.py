8bc39bcbfcec8d83a4cfda22d929d8c48e054a79
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-17 20:12:11 -0300

    Modificando o valor de um atributo com um método

--- cap_09/car.py
+++ cap_09/car.py
@@ -22,10 +22,25 @@ class Car():
         print("This car has {} miles on it.".format(self.odometer_reading))
 
 
+    def update_odometer(self, mileage):
+        """ 
+        Define o valor de leitura do hodômetro com o valor especificado.
+        Rejeita a alteração se for tentativa de definir um valor menor para o hodômetro.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
 
 # Modificando o valor de um atributo diretamente
 my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+# Modificando o valor de um atributo com um método
+my_new_car.update_odometer(50)
 my_new_car.read_odometer()
