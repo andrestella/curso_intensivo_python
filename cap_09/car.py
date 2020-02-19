866dbec5a0f97b8d27d206080dd84ba41a90ca07
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-18 22:48:58 -0300

    Criando a classe 'Battery' e alterando a classe 'ElectricCar'

--- cap_09/car.py
+++ cap_09/car.py
@@ -46,6 +46,47 @@ class Car():
         print("Filling the tank...")
 
 
+class Battery():
+    """ Uma tentativa simples de modelar uma bateria para um carro elétrico. """
+
+
+    def __init__(self, battery_size=70):
+        """ Inicializa os atributos da bateria. """
+        self.battery_size = battery_size
+
+
+    def describe_battery(self):
+        """ Exibe uma frase que exibe a capacidade da bateria. """
+        print("This car has a {}-kWh battery.".format(self.battery_size))
+
+
+    def get_range(self):
+        """ Exibe uma frase sobre a distância que o carro é capaz de percorrer
+        com essa bateria. """
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        else:
+            range = "Not specified"
+
+        if range == "Not specified":
+            message = "Battery not specified."
+        else:
+            message = "This car can go approximately " + str(range)
+            message += " miles on a full charge."
+        print(message)
+
+
+    def upgrade_battery(self):
+        """ Verifica a capacidade da bateria e faz um upgrade se o valor for diferente de 85. """
+        if self.battery_size != 85:
+            self.battery_size = 85
+            print("Upgraded the battery to 85 kWh.")
+        else:
+            print("The battery is already upgraded.")
+
+
 # Quando criamos uma classe-filha, a classe pai deve fazer parte do arquivo atual 
 # e deve aparecer antes da classe-filha no arquivo.
 class ElectricCar(Car):
@@ -58,12 +99,10 @@ class ElectricCar(Car):
         Em seguida, inicializa os atributos específicos de um carro elétrico.
         """
         super().__init__(make, model, year)
-        self.battery_size = 70
-
-
-    def describe_battery(self):
-        """ Exibe uma frase que exibe a capacidade da bateria. """
-        print("This car has a {}-kWh battery.".format(self.battery_size))
+        self.battery = Battery()
+        # usando uma instância de 'Battery' como atributo da classe 'ElectricCar'
+        # qualquer instância de 'ElectricCar' agora terá uma instância de 'Battery'
+        # criada automaticamente
 
 
     # Sobrescrevendo métodos da classe-pai
@@ -99,5 +138,9 @@ my_used_car.fill_gas_tank()
 # Testando se a herança está funcionando 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+
+my_tesla.battery.describe_battery()
 my_tesla.fill_gas_tank()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
