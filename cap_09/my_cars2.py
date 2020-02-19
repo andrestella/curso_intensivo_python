0bd183179bde69cac5fcc8b0ff14a2844ed36c3d
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-18 23:53:09 -0300

    Incluindo uma observação no programa my_cars2.py

--- cap_09/my_cars2.py
+++ cap_09/my_cars2.py
@@ -6,3 +6,7 @@ print(my_beetle.get_descriptive_name())
 
 my_tesla = car.ElectricCar('tesla', 'roadster', 2016)
 print(my_tesla.get_descriptive_name())
+
+# A opção abaixo:
+# from car import *
+# também vai importar todas as classes do módulo, mas não é um método recomendado
