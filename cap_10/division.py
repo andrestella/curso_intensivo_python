100db71ad3c797dda13c6c607779f4df300701ae
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-20 19:35:56 -0300

    Incluindo o programa division.py

---
+++ cap_10/division.py
@@ -0,0 +1,19 @@
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by 0!")
+    except ValueError:
+        print("Insert numbers only!")
+    else: # qualquer código que dependa do bloco 'try' executar com sucesso
+    # deve ser colocado no bloco 'else'
+        print(answer)
