8b35f8a7d2f255bcc2e671274f38b2f5a531b03a
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-20 22:24:20 -0300

    Adicionando um laço 'while' no programa adicao.py

--- cap_10/adicao.py
+++ cap_10/adicao.py
@@ -1,10 +1,19 @@
-print("Informe dois números para eu fornecer a soma dos mesmos.")
-try:
-    first_number = int(input("Primeiro número: "))
-    second_number = int(input("Segundo número: "))
-except ValueError:
-    print("Informe apenas números!")    
-else:
-    sum_numbers = first_number + second_number
-    print("A soma de {} com {} resulta em {}."
-        .format(first_number, second_number, sum_numbers))
+print("Calculadora para adição. Digite 'q' para sair.")
+
+while True:
+    print("\nInforme dois números para eu fornecer a soma dos mesmos.")
+    try:
+        first_number = input("Primeiro número: ")
+        if first_number == 'q':
+            break
+        first_number = int(first_number)
+        second_number = input("Segundo número: ")
+        if second_number == 'q':
+            break
+        second_number = int(second_number)
+    except ValueError:
+        print("Informe apenas números!")    
+    else:
+        sum_numbers = first_number + second_number
+        print("A soma de {} com {} resulta em {}."
+            .format(first_number, second_number, sum_numbers))
