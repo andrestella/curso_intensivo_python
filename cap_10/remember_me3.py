6d3bc52b3beb86a3c22c676823b017bdaa2800fa
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-22 16:03:44 -0300

    Incluindo a função check_username()

--- cap_10/remember_me3.py
+++ cap_10/remember_me3.py
@@ -4,7 +4,7 @@ import json
 
 def get_stored_username():
     """ Obtém o nome do usuário já armazenado se estiver disponível. """
-    filename = 'username3.json'
+    filename = 'username4.json'
     try:
         with open(filename) as f_obj:
             username = json.load(f_obj)
@@ -19,21 +19,27 @@ def get_stored_username():
 def get_new_username():
     """ Pede um novo nome de usuário. """
     username = input("What is your name? ")
-    filename = 'username3.json'
+    filename = 'username4.json'
     with open(filename, 'w') as f_obj:
             json.dump(username, f_obj)
     return username
+
+def check_username(username):
+    """ Verificar se o nome do usuário está correto. """
+    # Inclusão da função para evitar as instruções 'if' aninhadas da
+    # versão anterior.
+    correct = input("Are you {}? (yes/no) ".format(username))
+    if correct == 'yes':
+        return username
 
 def greet_user():
     """ Saúda o usuário pelo nome. """
     username = get_stored_username()
     if username:
-        correct = input("Are you {}? (yes/no) ".format(username))
-        if correct == 'yes':
-            print("Welcome back, {}!".format(username))
-            return
-            # Uma declaração de retorno vazia informa ao Python para deixar
-            # a função sem executar mais nenhum código dentro dela.
+        username = check_username(username)
+    if username:        
+        print("Welcome back, {}!".format(username))
+        return
     username = get_new_username()
     print("We'll remember you when you come back, {}!".format(username))
     
