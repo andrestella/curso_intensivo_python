21a08bcf3340182973856483ee7ebe50946e894e
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-22 14:46:17 -0300

    Refatorando o programa remember_me3.py

--- cap_10/remember_me3.py
+++ cap_10/remember_me3.py
@@ -2,18 +2,29 @@
 
 import json
 
-def greet_user():
-    """ Saúda o usuário pelo nome. """
+def get_stored_username():
+    """ Obtém o nome do usuário já armazenado se estiver disponível. """
     filename = 'username2.json'
     try:
         with open(filename) as f_obj:
-            username = json.load(f_obj)        
+            username = json.load(f_obj)
     except FileNotFoundError:
+        return None
+        # Essa é uma boa prática: uma função deve devolver o valor esperado
+        # ou 'None'. Isso nos permite fazer um teste simples com o valor de
+        # retorno da função.
+    else:
+        return username
+
+def greet_user():
+    """ Saúda o usuário pelo nome. """
+    username = get_stored_username()
+    if username:
+        print("Welcome back, {}!".format(username))
+    else:        
         username = input("What is your name? ")
         with open(filename, 'w') as f_obj:
             json.dump(username, f_obj)
             print("We'll remember you when you come back, {}!".format(username))
-    else:
-        print("Welcome back, {}!".format(username))
-
+    
 greet_user()
