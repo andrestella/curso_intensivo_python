de3d67b102337ed0c8c693c1b5878191ce2e5020
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-23 11:31:27 -0300

    Ajustando as alterações feitas para receber dados da população

--- cap_11/city_functions.py
+++ cap_11/city_functions.py
@@ -1,7 +1,10 @@
 """ Uma coleção de funções para trabalhar com cidades. """
 
 
-def city_data(cidade, pais, populacao):
-    """ Devolve o nome da cidade junto de seu país e população de forma elegante. """
-    cidade_dados = cidade + ", " + pais + " - população " + populacao
+def city_data(cidade, pais, populacao=''):
+    """ Devolve os dados informados da cidade de forma elegante. """
+    if populacao:
+        cidade_dados = cidade + ", " + pais + " - população " + populacao
+    else:
+        cidade_dados = cidade + ", " + pais
     return cidade_dados.title()
