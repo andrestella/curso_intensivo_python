0c7cded37c1920f5b4af3769625f95c5435f0581
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-23 10:34:19 -0300

    Incluído mais um teste no programa test_name_function.py

--- cap_11/test_name_function.py
+++ cap_11/test_name_function.py
@@ -6,12 +6,23 @@ class NamesTestCase(unittest.TestCase):
     """ Testes para 'name_function.py'. """
 
 
+    # Nomes longos para os métodos em nossas classes 'TestCase' não são
+    # um problema. Eles devem ser descritivos para que você possa compreender
+    # a saída quando seus testes falaharem, e pelo fato de Python os 
+    # chamar automaticamente, você não precisará escrever código para
+    # chamar esses métodos.
     def test_first_last_name(self):
     # Qualquer método que comece com 'test_' será executado de modo
     # automático quando 'test_name_function.py' for executado.
         """ Nomes como 'Janis Joplin' funcionam? """
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
+
+
+    def test_first_last_middle_name(self):
+        """ Nomes como 'Wolfgang Amadeus Mozart' funcionam? """
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
 
 
 unittest.main()
