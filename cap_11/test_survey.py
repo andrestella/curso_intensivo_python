b9e60bed85cf5b1802a6eeba5565abb41ae20508
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2020-02-24 21:10:18 -0300

    Incluído o método setUp() no programa test_survey.py

--- cap_11/test_survey.py
+++ cap_11/test_survey.py
@@ -6,23 +6,37 @@ class TestAnonymousSurvey(unittest.TestCase):
     """ Testes para a classe AnonymousSurvey. """
 
 
+    # A classe 'unittest.TestCase' tem um método 'setUp()' que permite 
+    # criar os objetos uma vez e então usá-los em cada um de seus
+    # métodos de teste. Quando um método 'setUp()' é incluído em uma
+    # classe 'TestCase', Python executa esse método antes de qualquer
+    # método cujo nome comece com 'test_'. Qualquer objeto criado no
+    # método 'setUp()' estará disponível a todos os métodos de teste
+    # que você escrever.
+    def setUp(self):
+        """
+        Cria uma pesquisa e um conjunto de respostas que poderão ser usados
+        em todos os métodos de teste.
+        """
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+        # Cada um desses dados é prefixado com 'self' para que possam
+        # ser usados em qualquer lugar da classe.
+
+
     def test_store_single_response(self):
         """ Testa se uma única resposta é armazenada de forma apropriada. """
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
 
     def test_store_three_responses(self):
         """ Testa se três respostas individuais são armazenadas de forma apropriada. """
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Spanish', 'Mandarin']
-        for response in responses:
-            my_survey.store_response(response)
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 
 unittest.main()
